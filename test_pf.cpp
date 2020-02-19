e519817f68844660961c4065a338464226af06ab
Mark Liu <uilkram@gmail.com>
2020-02-19 15:55:24 -0800

    plots

--- test_pf.cpp
+++ test_pf.cpp
@@ -4,7 +4,29 @@
 
 #include <iostream>
 
+namespace plt = matplotlibcpp;
+
 const int NUM_PARTICLES = 500;
+
+void PlotParticles(std::vector<Scene> &particles) {
+  // we only care about the object state for each particle.
+  std::vector<State> particleStates;
+  std::transform(
+      particles.begin(), particles.end(), std::back_inserter(particleStates),
+      [](const Scene &scene) { return scene.states.begin()->second; });
+  // Plot the s,v for each state.
+  std::vector<double> s(particleStates.size());
+  std::vector<double> v(particleStates.size());
+
+  for (int i = 0; i < particleStates.size(); i++) {
+    s[i] = particleStates[i].s;
+    v[i] = particleStates[i].v;
+  }
+  plt::xlabel("travel");
+  plt::ylabel("velocity");
+  plt::scatter(s, v);
+  plt::show();
+}
 
 int main() {
   ParticleFilter pf(NUM_PARTICLES);
@@ -20,6 +42,7 @@ int main() {
   pf.Init(scene);
   auto particles = pf.GetParticles();
   // what do our particles look like?
+  PlotParticles(particles);
   {
     // Let's evolve the scene at a dt of 0.5
     // Assume the actor is in same relative position, but we
