c89b8191694ad915d55ad7baaa81c0cdf69b4503
Mark Liu <uilkram@gmail.com>
2020-02-20 15:11:06 -0800

    stub argument for saving data to file

--- test_pf.cpp
+++ test_pf.cpp
@@ -2,12 +2,17 @@
 #include "particle_filter.h"
 #include "plotting.h"
 
+#include <string.h>
 #include <iostream>
 #include <unordered_map>
 
 const int NUM_PARTICLES = 5000;
 
-int main() {
+int main(int argc, char *argv[]) {
+  bool saveToFile = argc != 1 && strcmp(argv[1], "save") == 0;
+  if (saveToFile) {
+      std::cout << "todo: save to file!" << std::endl;
+  }
   ParticleFilter pf(NUM_PARTICLES);
   Scene scene(State(/*s=*/0,
                     /*v=*/20,
@@ -33,8 +38,8 @@ int main() {
     double dt = 0.5;
     Scene updatedScene = scene;
 
-    Strategy objectStrategy = Strategy::MAX_ACCEL_LATE;
-    // Strategy objectStrategy =Strategy::ACCEL_THEN_DECEL_LATE
+    // Strategy objectStrategy = Strategy::MAX_ACCEL_LATE;
+    Strategy objectStrategy =Strategy::ACCEL_THEN_DECEL_LATE;
     EvolveScene(dt, &updatedScene,
                 /*egoStrategy=*/Strategy::CONSTANT_ACCEL, objectStrategy);
 
